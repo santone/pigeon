cc652031d623a5d1e40bdc1eeb13aea4ee126564
Janos Balint <janos.balint@abiscon.com>
2022-12-13 19:15:57 +0100

    vanila

---
+++ src/test/java/hu/santone/pigeon/PigeonApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.santone.pigeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
